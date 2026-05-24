5b010e98bc40f7824805f3dde897f5e45306a2ea
varshini <varshiniyamala05@gmail.com>
2026-05-24 19:56:39 +0530

    fixed final cors

--- backend/index.js
+++ backend/index.js
@@ -21,9 +21,10 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      process.env.FRONTEND_URL
+      "https://snap-study-six.vercel.app",
     ],
-    credentials: true
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
   })
 );
 
