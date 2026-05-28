d9e3894055912df33d2d53a570d871feb062adb8
varshini <varshiniyamala05@gmail.com>
2026-05-28 16:26:09 +0530

    fixed gmail smtp timeout

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,15 @@ const nodemailer = require("nodemailer");
 const crypto = require("crypto");
 const User = require("./models/User");
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   }
+
 });
 
 const express = require("express");
